d91b1f4a9e831fc9743620e2c3562c7950465812
Bananahemic <bananahemic@users.noreply.github.com>
2018-05-16 12:39:27 -0700

    Minor refractor

--- gst-srt/gstsrt.c
+++ gst-srt/gstsrt.c
@@ -34,8 +34,8 @@
 GST_DEBUG_CATEGORY (GST_CAT_DEFAULT);
 
 SRTSOCKET
-gst_srt_client_connect_full (GstElement * elem, int sender,
-  const gchar * host, guint16 port, int rendezvous,
+gst_srt_client_connect_full (GstElement * elem, gboolean is_sender,
+  const gchar * host, guint16 port, gboolean rendezvous,
   const gchar * bind_address, guint16 bind_port, int latency,
   GSocketAddress ** socket_address, gint * poll_id, gchar * passphrase,
   int key_length)
@@ -60,6 +60,15 @@ gst_srt_client_connect_full (GstElement * elem, int sender,
     goto failed;
   }
 
+  *poll_id = srt_epoll_create ();
+  GST_INFO_OBJECT (elem, "SRT Epoll Created %i", *poll_id);
+  if (*poll_id == -1) {
+    GST_ELEMENT_ERROR (elem, LIBRARY, INIT, (NULL),
+      ("failed to create poll id for SRT socket (reason: %s)",
+        srt_getlasterror_str ()));
+    goto failed;
+  }
+
   sa_len = g_socket_address_get_native_size (*socket_address);
   sa = g_alloca (sa_len);
   if (!g_socket_address_to_native (*socket_address, sa, sa_len, &error)) {
@@ -71,10 +80,12 @@ gst_srt_client_connect_full (GstElement * elem, int sender,
   GSocketFamily address_family = g_socket_address_get_family (*socket_address);
   if (address_family == AF_INET)
       GST_LOG_OBJECT (elem, "Using IPv4");
-  else if(address_family == AF_INET6)
+  else if (address_family == AF_INET6)
       GST_WARNING_OBJECT (elem, "Using IPv6 with SRT, this is not fully supported");
-  else if(address_family == AF_UNIX)
+  else if (address_family == AF_UNIX)
       GST_WARNING_OBJECT (elem, "Using Unix socket with SRT, this is not fully supported");
+  else
+      GST_WARNING_OBJECT (elem, "Unknown address family");
 
   // The SOCK_DGRAM and 0 are legacy from UDT and are not used
   sock = srt_socket (address_family, SOCK_DGRAM, 0);
@@ -85,27 +96,32 @@ gst_srt_client_connect_full (GstElement * elem, int sender,
     goto failed;
   }
 
+  int on = 1;
+  int off = 0;
   /* Make sure TSBPD mode is enable (SRT mode) */
-  srt_setsockopt (sock, 0, SRTO_TSBPDMODE, &(int) {
-    1}, sizeof (int));
+  srt_setsockopt (sock, 0, SRTO_TSBPDMODE, &on, sizeof (int));
 
   /* srt recommends disabling linger */
-  srt_setsockopt (sock, 0, SRTO_LINGER, &(int) {
-    0}, sizeof (int));
+  srt_setsockopt (sock, 0, SRTO_LINGER, &off, sizeof (int));
 
   /* If this is a sink, we're a sender, otherwise we're a receiver */
-  srt_setsockopt (sock, 0, SRTO_SENDER, &sender, sizeof (int));
+  int is_sender_int = (int)is_sender;
+  srt_setsockopt (sock, 0, SRTO_SENDER, &is_sender_int, sizeof (int));
 
   // If we're a sender, latency is the minimum latency for the receiver
   // If we're a receiver, it's our latency
-  if(sender)
+  if(is_sender)
       srt_setsockopt (sock, 0, SRTO_PEERLATENCY, &latency, sizeof (int));
   else
       srt_setsockopt (sock, 0, SRTO_RCVLATENCY, &latency, sizeof (int));
 
-  srt_setsockopt (sock, 0, SRTO_RENDEZVOUS, &rendezvous, sizeof (int));
+  GST_INFO_OBJECT (elem, "Using as latency: %i", latency);
+
+  int rendezvousInt = (int)rendezvous;
+  srt_setsockopt (sock, 0, SRTO_RENDEZVOUS, &rendezvousInt, sizeof (int));
 
   if (passphrase != NULL && passphrase[0] != '\0') {
+    GST_INFO_OBJECT (elem, "Using passphrase");
     srt_setsockopt (sock, 0, SRTO_PASSPHRASE, passphrase, (int)strlen (passphrase));
     srt_setsockopt (sock, 0, SRTO_PBKEYLEN, &key_length, sizeof (int));
   }
@@ -150,24 +166,15 @@ gst_srt_client_connect_full (GstElement * elem, int sender,
     }
   }
 
-  *poll_id = srt_epoll_create ();
-  GST_INFO_OBJECT (elem, "SRT Epoll Created");
-  if (*poll_id == -1) {
-    GST_ELEMENT_ERROR (elem, LIBRARY, INIT, (NULL),
-      ("failed to create poll id for SRT socket (reason: %s)",
-        srt_getlasterror_str ()));
-    goto failed;
-  }
-
   int connectRet = srt_connect (sock, sa, (int)sa_len);
   if (connectRet == SRT_ERROR) {
     GST_ELEMENT_ERROR (elem, RESOURCE, OPEN_READ, ("Connection error"),
       ("failed to connect to host (reason: %s)", srt_getlasterror_str ()));
     goto failed;
   }
-  GST_INFO_OBJECT (elem, "SRT Returned %i", connectRet);
+  GST_INFO_OBJECT (elem, "SRT connect returned %i", connectRet);
 
-  int events = sender ? SRT_EPOLL_IN | SRT_EPOLL_OUT | SRT_EPOLL_ERR
+  int events = is_sender ? SRT_EPOLL_IN | SRT_EPOLL_OUT | SRT_EPOLL_ERR
     : SRT_EPOLL_IN | SRT_EPOLL_ERR;
   int addUsockRet = srt_epoll_add_usock (*poll_id, sock, &events);
   GST_INFO_OBJECT (elem, "SRT Epoll Has Usock Added. Returned: %i", addUsockRet );
